d3253e5ea4fc7323717fc2300cdbc5625b5cbac0
Ashwin <pokharelashwin@hotmail.com>
2022-09-16 23:24:52 +1000

    cash java

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let subscribe = false
-let loggedIn = true
+// let subscribe = false
+// let loggedIn = true
 
-if (subscribe  === true){
-    console.log("Show the video")
+// if (subscribe  === true){
+//     console.log("Show the video")
+// }
+// else if(loggedIn === true){
+//     console.log("tell user to upgrade their subscription")
+// }
+// else{
+//     console.log("tell user to log into account")
+// }
+
+let cash = 50
+let price = 40
+
+if (cash > price){
+    console.log("You paid extra - here is your change")
+    
 }
-else if(loggedIn === true){
-    console.log("tell user to upgrade their subscription")
+else if ( cash == price){
+    console.log("have a nice day")
 }
 else{
-    console.log("tell user to log into account")
+    console.log( "give me more money")
 }
